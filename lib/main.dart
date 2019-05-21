dae5fca92a0e2e35a3ec226698268c1e85558ca1
Túlio <francatm@outlook.com>
2019-05-21 01:01:07 -0300

    Add gesture detection with button widgets

--- lib/main.dart
+++ lib/main.dart
@@ -16,15 +16,27 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        ),
-        Expanded(
-          child: Image.asset('images/dice6.png'),
-        ),
-      ],
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice1.png'),
+              onPressed: () {
+                print('left button pressed');
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice6.png'),
+              onPressed: () {
+                print('right button pressed');
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
