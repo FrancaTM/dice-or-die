ec75cda7cf1c76d9fe6df4590a0c0ee6109bce48
Túlio <francatm@outlook.com>
2019-05-20 22:09:56 -0300

    Add expanded widgets and images

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,15 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Image.asset('images/dice6.png'),
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Image.asset('images/dice1.png'),
+        ),
+        Expanded(
+          child: Image.asset('images/dice6.png'),
+        ),
+      ],
     );
   }
 }
